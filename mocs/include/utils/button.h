cd325aea7869065cb85a3bddcdc99f7e47d3f2b1
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-12-22 23:33:42 +0530

    define button class

---
+++ mocs/include/utils/button.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <Arduino.h>
+
+class Button{
+    private:
+        const uint8_t pin;
+        bool state;
+        bool previous;
+        bool current;
+
+    public:
+        void begin();
+        Button(const uint8_t pin);
+        void update();
+
+        bool isPressed();
+};
