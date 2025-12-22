ecb69aed1dc5a442104a055e6ab8de9337673cf2
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-12-22 23:23:40 +0530

    implement the led class

---
+++ mocs/src/utils/led.cpp
@@ -0,0 +1,40 @@
+#include "utils/led.h"
+
+LED::LED(const uint8_t pin,unsigned long duration):
+    pin(pin),
+    duration(duration),
+    startTime(0),
+    shouldBlink(false),
+    status(false){}
+
+void LED::begin(){
+    pinMode(pin,OUTPUT);
+    setState(false);
+}
+
+void LED::setDuration(unsigned long duration){
+    this->duration = duration;
+}
+
+void LED::setShouldBlink(bool blink){
+    if(shouldBlink != blink){
+        this->shouldBlink = blink;
+        startTime = millis();
+    }
+}
+
+void LED::setState(bool state){
+    this->status = state;
+    digitalWrite(pin,status);
+}
+
+void LED::update(){
+    unsigned long now = millis();
+    if(shouldBlink){
+        if(now - startTime >= duration){
+            startTime = now;
+            status = !status;
+            digitalWrite(pin,status);
+        }
+    }
+}
