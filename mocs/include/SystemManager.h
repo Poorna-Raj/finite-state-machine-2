90a72e4df2ea623a91592a2218775aa4290149ae
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-12-27 13:08:14 +0530

    define and implement the state manager

---
+++ mocs/include/SystemManager.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "utils/button.h"
+#include "utils/led.h"
+#include "SystemState.h"
+
+class SystemManager
+{
+private:
+    unsigned long now;
+    unsigned long startTime;
+    Button *modeButton;
+    Button *resetButton;
+    LED *stateLED;
+    LED *diagLED;
+    SystemState state;
+    SystemState prevState;
+
+public:
+    void begin();
+    SystemManager(
+        Button *mB,
+        Button *rB,
+        LED *sL,
+        LED *dL);
+
+    void update();
+    void onEnterState(SystemState state);
+
+    void setState(SystemState state);
+    void updateModeButton();
+    void updateResetButton();
+    void updateModeResetButton();
+};
---
+++ mocs/src/SystemManager.cpp
@@ -0,0 +1,137 @@
+#include "SystemManager.h"
+
+SystemManager::SystemManager(
+    Button *mB,
+    Button *rB,
+    LED *sL,
+    LED *dL)
+    : modeButton(mB), resetButton(rB), stateLED(sL), diagLED(dL), state(SystemState::BOOT), startTime(0), prevState(SystemState::BOOT)
+{
+}
+
+void SystemManager::begin()
+{
+    modeButton->begin();
+    resetButton->begin();
+    stateLED->begin();
+    diagLED->begin();
+}
+
+void SystemManager::update()
+{
+    now = millis();
+
+    modeButton->update();
+    resetButton->update();
+    stateLED->update();
+    diagLED->update();
+
+    if (state == SystemState::BOOT)
+    {
+        if (now - startTime >= 5000)
+        {
+            setState(SystemState::ACTIVE);
+        }
+    }
+
+    if (state == SystemState::RECOVERY)
+    {
+        if (now - startTime >= 2000)
+        {
+            setState(SystemState::IDLE);
+        }
+    }
+
+    if (state != prevState)
+    {
+        onEnterState(state);
+        prevState = state;
+    }
+
+    updateModeResetButton();
+    updateModeButton();
+    updateResetButton();
+}
+
+void SystemManager::setState(SystemState state)
+{
+    this->state = state;
+}
+
+void SystemManager::onEnterState(SystemState state)
+{
+    startTime = millis();
+    switch (state)
+    {
+    case SystemState::BOOT:
+        stateLED->setDuration(1000);
+        stateLED->setShouldBlink(true);
+        diagLED->setShouldBlink(false);
+        diagLED->setState(false);
+        break;
+    case SystemState::IDLE:
+        stateLED->setShouldBlink(false);
+        stateLED->setState(false);
+        diagLED->setDuration(1000);
+        diagLED->setShouldBlink(true);
+        break;
+    case SystemState::ACTIVE:
+        stateLED->setShouldBlink(false);
+        stateLED->setState(true);
+        diagLED->setShouldBlink(false);
+        diagLED->setState(false);
+        break;
+    case SystemState::ERROR:
+        stateLED->setShouldBlink(true);
+        stateLED->setDuration(200);
+        diagLED->setShouldBlink(true);
+        diagLED->setDuration(200);
+        break;
+    case SystemState::RECOVERY:
+        stateLED->setShouldBlink(false);
+        stateLED->setState(false);
+        diagLED->setShouldBlink(false);
+        diagLED->setState(true);
+        break;
+    default:
+        Serial.println("Unknown State");
+        break;
+    }
+}
+
+void SystemManager::updateModeButton()
+{
+    if (modeButton->isPressed())
+    {
+        if (state == SystemState::ACTIVE)
+        {
+            setState(SystemState::IDLE);
+        }
+        else if (state == SystemState::IDLE)
+        {
+            setState(SystemState::ACTIVE);
+        }
+    }
+}
+
+void SystemManager::updateResetButton()
+{
+    if (resetButton->isPressed())
+    {
+        if (state == SystemState::ERROR)
+        {
+            setState(SystemState::RECOVERY);
+        }
+    }
+}
+
+void SystemManager::updateModeResetButton()
+{
+    if (modeButton->isPressed() && resetButton->isPressed())
+    {
+        if (state == SystemState::ACTIVE || state == SystemState::IDLE)
+        {
+            setState(SystemState::ERROR);
+        }
+    }
+}
